acc8bf41e118ed6a0a348b54527e3b1bf623cced
Gautam <gowtham_abbavaram@epam.com>
2017-03-10 17:53:58 +0530

    initial commit

---
+++ app/routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from "@angular/router";
+import { Error404Component } from "./errors/404.component";
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+ } from "./events/index";
+
+export const appRoutes = [
+  // create new event
+  { path: "events/new",
+    component: CreateEventComponent,
+    canDeactivate: ["canDeactivateCreateEvent"] },
+  // events
+  { path: "events",
+    component: EventsListComponent,
+    resolve: {events: EventListResolver} },
+  // events/1
+  // events/foo
+  { path: "events/:id",
+    component: EventDetailsComponent,
+    canActivate: [EventRouteActivator] },
+  // error page
+  { path: "404",
+    component: Error404Component },
+  // default route
+  { path: "",
+    redirectTo: "/events",
+    pathMatch: "full"},
+
+  { path: "user", loadChildren: "app/user/user.module#UserModule"}
+];
